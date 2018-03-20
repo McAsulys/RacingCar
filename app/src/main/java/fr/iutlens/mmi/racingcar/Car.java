409e2b1c36961f1c9a0651d4db59125fc2ae5a33
benedict.dourlens <benedict.dourlens@gmail.com>
2018-03-20 09:21:10 +0100

    Fin du code de déplacement

--- app/src/main/java/fr/iutlens/mmi/racingcar/Car.java
+++ app/src/main/java/fr/iutlens/mmi/racingcar/Car.java
@@ -14,16 +14,20 @@ public class Car {
 
     private SpriteSheet sprite;
 
-    float x,y,direction;
-    float v,dd;
+    float x,y,direction,a,vMax, vMin, vflow, vTarget;
+    float vy,dd;
 
-    public Car(int sprite_id, float x, float y, float direction){
+    public Car(int sprite_id, float x, float y){
         this.x = x;
         this.y = y;
-        this.direction = direction;
+        this.direction = 0;
+        this.a = 0;
         this.sprite = SpriteSheet.get(sprite_id);
-        this.v = 0;
+        this.vy = (float) 0.01;
         this.dd = 0;
+        this.vMax = (float) 1;
+        this.vMin = 0;
+        this.vflow = (float) 0.1;
     }
 
     public void paint(Canvas canvas, int unit_x, int unit_y){
@@ -39,10 +43,24 @@ public class Car {
 
 
     public void update() {
-        direction += dd*v*sprite.h;
-        x += (float) ( v*sprite.h*Math.cos(Math.toRadians(direction-90)));
-        y += (float) ( v*sprite.h*Math.sin(Math.toRadians(direction-90)));
-        //y += (float) (y+v+0.5(a));
+        //direction += dd*vy*sprite.h;
+        //x += (float) ( vy*sprite.h*Math.cos(Math.toRadians(direction-90)));
+        //y += (float) ( vy*sprite.h*Math.sin(Math.toRadians(direction-90)));
+
+        a = 0.001f;
+        float dv = vTarget- vy;
+        if (dv>0 && dv>a){
+            dv = a;
+        } else if (dv <0 && dv < -a){
+            dv =-10*a;
+        }
+        vy = vy +dv;
+        y = y- vy;
+
+        double vx = this.dd * 0.003;
+        x -= vx;
+
+        direction = (float) Math.toDegrees(Math.atan2(vy,vx/5))-90;
 
 
     }
@@ -60,12 +78,21 @@ public class Car {
     }
 
     public void setCommand(double pitch, double roll) {
+        pitch -= 45;
         pitch = rescale(pitch,90,15);
         roll = rescale(roll,90,15);
 
-        this.v = (float) (pitch*0.00005);
+        this.a = (float) (pitch*0.00005);
         this.dd = 0;
         this.dd = (float) (roll/2);
+        if (pitch < 0){
+            //droite entre 0 et vflow sur -90 a 0
+            this.vTarget = (float) (-pitch*0.01+vflow);
+        }else {
+            //droite entre vflow et vMax sur 0 90
+            this.vTarget = (float) (-pitch*0.001+vflow);
+        }
+        //this.vTarget = (float) (pitch*0.001)+vflow;
 
     }
 }
